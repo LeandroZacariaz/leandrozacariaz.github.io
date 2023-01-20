e1c040de019dfa0cbd99d933a7d4fb09035cd77e
LeandroZacariaz <123131678+LeandroZacariaz@users.noreply.github.com>
2023-01-19 21:48:02 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let hideTextProf= document.getElementById('hideTextProf');
+document.getElementById('botonProf').onclick = function(){
+    hideTextProf.classList.toggle('show');
+}
+
+let hideTextExp= document.getElementById('hideTextExp');
+document.getElementById('botonExp').onclick = function(){
+    hideTextExp.classList.toggle('show');
+}
+
+let hideTextEdu= document.getElementById('hideTextEdu');
+document.getElementById('botonEdu').onclick = function(){
+    hideTextEdu.classList.toggle('show');
+}
+
+let hideTextHab= document.getElementById('hideTextHab');
+document.getElementById('botonHab').onclick = function(){
+    hideTextHab.classList.toggle('show');
+}
+
+let hideTextIdio= document.getElementById('hideTextIdio');
+document.getElementById('botonIdio').onclick = function(){
+    hideTextIdio.classList.toggle('show');
+}
+
+
